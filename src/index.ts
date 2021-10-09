2f6fca3919a04c34a900fb3fa94172e4da06040b
Carlo Bagnoli <carlo.bagnoli.gomez@gmail.com>
2021-10-09 16:46:09 -0400

    feature: added the s combinator

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-/*
- * mcomp: Monoid COMPpsition, is simply the compose function
- */
 export const mcomp = <A, B, C>(f: (b: B) => C, g: (a: A) => B): ((a: A) => C) => x => f(g(x));
 export const comp = (...f: any[]) => f.reduce(mcomp);
 
 export const mpipe = <A, B, C>(f: (a: A) => B, g: (b: B) => C): ((a: A) => C) => mcomp(g, f);
 export const pipe = (...x: any[]) => x.reduce(pipe);
+
+export const id = <T>(x: T) => x;
+export const constant = <T>(x: T) => () => x;
+export const scomb = <A, B, C>(f: (a: A, b: B) => C, g: (a: A) => B, x: A): C => f(x, g(x));
