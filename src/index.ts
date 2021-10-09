13d2f7a3f8a05a99fda71665b4979c9bc67a1d34
Carlo Bagnoli <carlo.bagnoli.gomez@gmail.com>
2021-10-09 17:49:47 -0400

    added: folds and call functions

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ export const pipe = (...x: any[]) => x.reduce(pipe);
 
 export const id = <T>(x: T) => x;
 export const constant = <T>(x: T) => () => x;
+export const call = <T>(f: () => T) => f();
 
 export const scomb = <A, B, C>(f: (a: A, b: B) => C, g: (a: A) => B, x: A): C => f(x, g(x));
 export const cscomb = <A, B, C>(f: (a: A, b: B) => C) => (g: (a: A) => B) => (x: A): C => f(x, g(x));
@@ -16,3 +17,13 @@ export const first = <T>(x: T[]) => x[0];
 export const last  = <T>(x: T[]) => x.slice(-1)[0];
 export const rest  = <T>(x: T[]) => x.slice(1);
 export const init  = <T>(x: T[]) => x.slice(0, -1);
+
+export const foldl = <A, B>(x: A[], f: (a: A, b: B) => A, init: A) => Array.prototype.reduce.bind(x)(f, init);
+export const foldl1 = <A, B>(x: A[], f: (a: A, b: B) => A) => Array.prototype.reduce.bind(x)(f);
+export const cfoldl = <A, B>(x: A[]) => (f: (a: A, b: B) => A) => (init: A) => Array.prototype.reduce.bind(x)(f, init);
+export const cfoldl1 = <A, B>(x: A[]) => (f: (a: A, b: B) => A) => Array.prototype.reduce.bind(x)(f, init);
+
+export const foldr = <A, B>(x: A[], f: (a: A, b: B) => A, init: A) => Array.prototype.reduceRight.bind(x)(f, init);
+export const foldr1 = <A, B>(x: A[], f: (a: A, b: B) => A) => Array.prototype.reduceRight.bind(x)(f);
+export const cfoldr = <A, B>(x: A[]) => (f: (a: A, b: B) => A) => (init: A) => Array.prototype.reduceRight.bind(x)(f, init);
+export const cfoldr1 = <A, B>(x: A[]) => (f: (a: A, b: B) => A) => Array.prototype.reduceRight.bind(x)(f, init);
