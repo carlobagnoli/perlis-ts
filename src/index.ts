28b974b85e664c0a55fbde9a2bf461cbd3c47258
Carlo Bagnoli <carlo.bagnoli.gomez@gmail.com>
2021-10-09 17:03:28 -0400

    feature: added curried versions of comb and pipe and added array functions

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 export const mcomp = <A, B, C>(f: (b: B) => C, g: (a: A) => B): ((a: A) => C) => x => f(g(x));
+export const ccomp = <A, B, C>(f: (b: B) => C) => (g: (a: A) => B): ((a: A) => C) => x => f(g(x));
 export const comp = (...f: any[]) => f.reduce(mcomp);
 
 export const mpipe = <A, B, C>(f: (a: A) => B, g: (b: B) => C): ((a: A) => C) => mcomp(g, f);
+export const cpipe = <A, B, C>(f: (a: A) => B) => (g: (b: B) => C): ((a: A) => C) => mcomp(g, f);
 export const pipe = (...x: any[]) => x.reduce(pipe);
 
 export const id = <T>(x: T) => x;
 export const constant = <T>(x: T) => () => x;
+
 export const scomb = <A, B, C>(f: (a: A, b: B) => C, g: (a: A) => B, x: A): C => f(x, g(x));
+export const cscomb = <A, B, C>(f: (a: A, b: B) => C) => (g: (a: A) => B) => (x: A): C => f(x, g(x));
+
+export const first = <T>(x: T[]) => x[0];
+export const last  = <T>(x: T[]) => x.slice(-1)[0];
+export const rest  = <T>(x: T[]) => x.slice(1);
+export const init  = <T>(x: T[]) => x.slice(0, -1);
